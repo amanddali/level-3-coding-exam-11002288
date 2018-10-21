8a6c6d4620428b3306e14d652dbc1800c41fba40
amanddali <league@WTS22.attlocal.net>
2018-10-21 15:58:44 -0700

    no

--- test/HashMapCalculator.java
+++ test/HashMapCalculator.java
@@ -2,15 +2,15 @@ import java.util.HashMap;
 
 public class HashMapCalculator {
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	public int commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
 		int value = 0;
-		for (int i = 0; i < hashmap1.size(); i++) {
-			for (int j = 0; j < hashmap2.size(); j++) {
-				if (hashmap1.get(i).equals(hashmap2.get(j))) {
-					value++;
+		for (String map : hashmap1.keySet()) {
+			if (hashmap2.containsKey(map)) {
+				if (hashmap1.get(hashmap1.keySet())) {
+					value += 1;
 				}
 			}
 		}
